4b52b4421e2733a8c10417d677cc2f572d6c3158
edwinb-ai <developeredwin@gmail.com>
2020-11-17 02:13:39 +0000

    Deploying to gh-pages from  @ ce904b17400525f3190591e51b19b82e1793b5e2 🚀

--- utils.jl
+++ utils.jl
@@ -1,16 +1,74 @@
+using Dates
+using Random
+
 function hfun_bar(vname)
-  val = Meta.parse(vname[1])
-  return round(sqrt(val), digits=2)
+    val = Meta.parse(vname[1])
+    return round(sqrt(val), digits=2)
 end
 
 function hfun_m1fill(vname)
-  var = vname[1]
-  return pagevar("index", var)
+    var = vname[1]
+    return pagevar("index", var)
 end
 
 function lx_baz(com, _)
-  # keep this first line
-  brace_content = Franklin.content(com.braces[1]) # input string
-  # do whatever you want here
-  return uppercase(brace_content)
+    # keep this first line
+    brace_content = Franklin.content(com.braces[1]) # input string
+    # do whatever you want here
+    return uppercase(brace_content)
+end
+
+"""
+    {{blogposts}}
+Plug in the list of blog posts contained in the `/blog/` folder.
+"""
+function hfun_blogposts()
+    today = Dates.today()
+    curyear = year(today)
+    curmonth = month(today)
+    curday = day(today)
+
+    list = readdir("blog")
+    filter!(f -> endswith(f, ".md"), list)
+    sorter(p) = begin
+        ps  = splitext(p)[1]
+        url = "/blog/$ps/"
+        surl = strip(url, '/')
+        pubdate = pagevar(surl, :published)
+        if isnothing(pubdate)
+            return Date(Dates.unix2datetime(stat(surl * ".md").ctime))
+        end
+        return Date(pubdate, dateformat"d U Y")
+    end
+    sort!(list, by=sorter, rev=true)
+
+    io = IOBuffer()
+    write(io, """<ul class="blog-posts">""")
+    for (i, post) in enumerate(list)
+        if post == "index.md"
+            continue
+        end
+        ps  = splitext(post)[1]
+        write(io, "<li><span><i>")
+        url = "/blog/$ps/"
+        surl = strip(url, '/')
+        title = pagevar(surl, :title)
+        pubdate = pagevar(surl, :published)
+        if isnothing(pubdate)
+            date = "$curyear-$curmonth-$curday"
+        else
+            date = Date(pubdate, dateformat"d U Y")
+        end
+        write(io, """$date</i></span><a href="$url">$title</a>""")
+    end
+    write(io, "</ul>")
+    return String(take!(io))
+end
+
+function hfun_rndnumber()
+    io = IOBuffer()
+    rng = RandomDevice()
+    a = abs(rand(Random.seed!(rng), Int))
+    write(io, "<p>$a</p>")
+    return String(take!(io))
 end
